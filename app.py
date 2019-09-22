bb919a2282a667f3786b764b4a7bb20e8f79ef37
Daniel <dmconnol@buffalo.edu>
2019-09-21 21:14:47 -0400

    Added more stuff

--- app.py
+++ app.py
@@ -34,10 +34,11 @@ def determine_earthquake():
     latitude = float(data["latitude"])
     min_latitude =  -0.45+ latitude
     max_latitude = 0.45+ latitude
-    min_longitude = -.4 + longitude
-    max_longitude = .4+longitude
-    response = requests.get("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=1980-01-01&endtime=2000-01-02&maxlatitude={}&minlatitude={}&maxlongitude={}&minlongitude={}".format(max_latitude, min_latitude, max_longitude, min_longitude))
-    #print("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=1980-01-01&endtime=2000-01-02&maxlatitude={}&minlatitude={}&maxlongitude={}&minlongitude={}".format(max_latitude, min_latitude, max_longitude, min_longitude))
+    min_longitude = -.5 + longitude
+    max_longitude = .5+longitude
+    response = requests.get("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=1980-01-01&endtime=2000-01-02&maxlatitude={}&minlatitude={}&maxlongitude={}&minlongitude={}&minmagnitude=3".format(max_latitude, min_latitude, max_longitude, min_longitude))
+    print("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=1980-01-01&endtime=2000-01-02&maxlatitude={}&minlatitude={}&maxlongitude={}&minlongitude={}&minmagnitude=3".format(max_latitude, min_latitude, max_longitude, min_longitude))
+    
     response = response.json()
     x_vector = [0]*5
     for data in response["features"]:
@@ -58,12 +59,14 @@ def determine_earthquake():
     iam_token = "eyJraWQiOiIyMDE5MDUxMyIsImFsZyI6IlJTMjU2In0.eyJpYW1faWQiOiJpYW0tU2VydmljZUlkLWU5YWVlYzhmLTNiOTQtNDQ5NC04ZTE0LTI1YjdiNDAzZDg3MiIsImlkIjoiaWFtLVNlcnZpY2VJZC1lOWFlZWM4Zi0zYjk0LTQ0OTQtOGUxNC0yNWI3YjQwM2Q4NzIiLCJyZWFsbWlkIjoiaWFtIiwiaWRlbnRpZmllciI6IlNlcnZpY2VJZC1lOWFlZWM4Zi0zYjk0LTQ0OTQtOGUxNC0yNWI3YjQwM2Q4NzIiLCJzdWIiOiJTZXJ2aWNlSWQtZTlhZWVjOGYtM2I5NC00NDk0LThlMTQtMjViN2I0MDNkODcyIiwic3ViX3R5cGUiOiJTZXJ2aWNlSWQiLCJhY2NvdW50Ijp7InZhbGlkIjp0cnVlLCJic3MiOiI3NjE4ZGJlNjcxYzY0OTk3YmU3ZTkzMzU3Zjg3NjEzMyJ9LCJpYXQiOjE1NjkxMTEzNTcsImV4cCI6MTU2OTExNDk1NywiaXNzIjoiaHR0cHM6Ly9pYW0ubmcuYmx1ZW1peC5uZXQvb2lkYy90b2tlbiIsImdyYW50X3R5cGUiOiJ1cm46aWJtOnBhcmFtczpvYXV0aDpncmFudC10eXBlOmFwaWtleSIsInNjb3BlIjoiaWJtIG9wZW5pZCIsImNsaWVudF9pZCI6ImJ4IiwiYWNyIjoxLCJhbXIiOlsicHdkIl19.Ebiu1ISGAHEdV48HGN_ByTgXNkXC_PZ-JsnxoawIGB_zwF307CGMTfZ3WnuSl1A3Xs00KF-0oy5xVZ1iVPaduEorEgjY0gvzXGcA3UPXkaBQ8lqFsERUJgNRVkdOm5XbhYgsl2uSRsPNLj9px1gy9hsUnyYQpcKl6ywwnmIr3KHYzB1WQCOMiMEcQ-aAikjRVDYtNmjoWQf6NGG23r7rpl2bb4c2g_-q0-XehAuK_uBaDbZd1rAVdkgV7ac1-84cwjNmXNZ-7vOMWpFywgUSIlhBkt-TWJPCU8-5_lrmf6hwKJqDfefDyLCtjO30Iv1Lk29iuYPM_xBJqCVfG67rxQ"
     # NOTE: generate iam_token and retrieve ml_instance_id based on provided documentation	
     header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + iam_token, 'ML-Instance-ID': ml_instance_id}
-    
-    # NOTE: manually define and pass the array(s) of values to be scored in the next line
-    payload_scoring = {"input_data": [{"fields": ["0", "1", "2", "3", "4"], "values": [x_vector]}]}
 
-    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/v4/deployments/d7f03c4c-86a4-44b0-a286-355c9315d701/predictions', json=payload_scoring, headers=header)
-    return str(json.loads(response_scoring.text)["predictions"][0]["values"][0][0])
+    # NOTE: manually define and pass the array(s) of values to be scored in the next line
+    payload_scoring = {"input_data": [{"fields": ["0", "1", "2", "3", "4"], "values": [[18,10,1,0,1]]}]}
+
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/v4/deployments/1d20095e-acdc-4c53-a82c-07daacf2e3d7/predictions', json=payload_scoring, headers=header)
+
+    print(response_scoring.text)
+    return str(json.loads(response_scoring.text))
 
 @app.route('/address', methods = ["POST"])
 def process_address():
